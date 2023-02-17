59b37a550e71b7b92ce48b667aeb405f66ed92e5
Wenkang Qin <wkqin@outlook.com>
2023-02-17 13:49:07 +0800

    ssh tunel proxy

--- csr/ssh_tunel.py
+++ csr/ssh_tunel.py
@@ -3,5 +3,9 @@ import subprocess
 
 def ssh_tunel(private_file_path, host, port):
     ssh_command = 'ssh -i {private_file_path} -p {port} root@{host}'
-    subprocess.run(ssh_command.format(private_file_path=private_file_path,
-                                      host=host, port=port).split())
+    ssh_command = ssh_command.format(private_file_path=private_file_path,
+                                      host=host, port=port)
+    ssh_command = ssh_command.split()
+    ssh_command += ['-o', 'ProxyCommand=nc -X 5 -x localhost:1080 %h %p']
+    subprocess.run(ssh_command)
+    import pdb; pdb.set_trace()
